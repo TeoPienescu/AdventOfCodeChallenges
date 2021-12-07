1421e1c617df4ec981de5cc8e8c01b637b4f15fa
Teo <teo_pienescu@hotmail.com>
2021-12-07 16:42:28 +0000

    Advent of Code 2021 - day 7

--- src/main/kotlin/2021/day7/day7.kt
+++ src/main/kotlin/2021/day7/day7.kt
@@ -3,6 +3,8 @@ package day7
 import getFile
 import kotlin.math.abs
 
+typealias CrabPositions = List<Int>
+
 fun main(){
     val crabPositions = getFile(7).readText().split(",").map { it.toInt() }
 
@@ -10,17 +12,17 @@ fun main(){
     requireNotNull(maxValue)
     val rangeOfDestPositions = (0..maxValue)
 
-    val part1 = getMinFuel(part1 = true, rangeOfDestPositions, crabPositions)
-    val part2 = getMinFuel(part1 = false, rangeOfDestPositions, crabPositions)
+    val part1 = crabPositions.getMinFuel(part1 = true, rangeOfDestPositions)
+    val part2 = crabPositions.getMinFuel(part1 = false, rangeOfDestPositions)
 
     println("Part1: $part1")
     println("Part1: $part2")
 
 }
 
-fun getMinFuel(part1: Boolean, rangeOfDestPositions: IntRange, crabPositions: List<Int>): Int{
+fun CrabPositions.getMinFuel(part1: Boolean, rangeOfDestPositions: IntRange): Int{
     return rangeOfDestPositions.fold(0){ acc, finalPos->
-        val fuel = crabPositions.fold(0){accm, xPos ->
+        val fuel = this.fold(0){accm, xPos ->
             val absValueOfCrabPosAndDestPos = abs(xPos - finalPos)
             accm + if(!part1) (0..absValueOfCrabPosAndDestPos).sum() else absValueOfCrabPosAndDestPos
         }
